a76661e47f715019b91b1390bb62e9bb53991174
Paras Rawat <parasrawatjr@gmail.com>
2020-08-05 21:18:06 +0530

    uploaded project.
    Changes to be made

---
+++ run.py
@@ -0,0 +1,20 @@
+from demo import *
+
+lines = [
+    "Im Paras"
+]
+
+biases = [.75 for i in lines]
+styles = [9 for i in lines]
+stroke_colors = ['red', 'green', 'black', 'blue']
+stroke_widths = [1, 2, 1, 2]
+
+hand = Hand()
+hand.write(
+    filename='img/usage_demo.svg',
+    lines=lines,
+    biases=biases,
+    styles=styles,
+    stroke_colors=stroke_colors,
+    stroke_widths=stroke_widths
+)
